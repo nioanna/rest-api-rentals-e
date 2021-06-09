4ee4f33837ce611322e7dc45faa72a8dd7be65da
Ivana <nioanna@outlook.com>
2021-06-09 15:43:50 +0200

    pokucah

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
@@ -155,7 +155,8 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    //'domain' => env('SESSION_DOMAIN', null),
+    'domain' => '.http://127.0.0.1:8000',
 
     /*
     |--------------------------------------------------------------------------
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
 Route::post("user-signup", [UserController::class, "userSignUp"]);
 Route::post("user-login", [UserController::class, "userLogin"]);
 Route::get("user/{email}", [UserController::class, "userDetail"]);
@@ -34,6 +40,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::put('homes/{residentialBuilding}', [HomesController::class, 'update']);
     Route::delete('homes/{residentialBuilding}', [HomesController::class, 'delete']);
 });
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
